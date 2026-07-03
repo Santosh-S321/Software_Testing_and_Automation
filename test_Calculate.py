cd7ea572be8b2df687868bfb83f4c975ae136154
Santosh-S321 <Santoshshankar7473@gmail.com>
2026-07-03 22:53:18 +0530

    Update test_Calculate.py

--- test_Calculate.py
+++ test_Calculate.py
@@ -1,10 +1,10 @@
 import pytest
 from Calculate import add, divide
 def test_add():
-    assert add(10, 30) == 30
+    assert add(10, 20) == 30
 
 def test_divide():
-    assert divide(20, 45) == 5
+    assert divide(20, 4) == 5
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
